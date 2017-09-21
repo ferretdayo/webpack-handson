769c32664bed201b47ff69f97ebf6d0d27911ae1
Tsuruda <ferretdayo@hotmail.co.jp>
2017-09-21 22:17:53 +0900

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,6 @@
-let taxCalc = require('./tax-calculator')
+let taxCalculator = require('./tax-calculator')
 
 let originalPrice = 1000
 let tax = 1.08
-let price = taxCalc(originalPrice, tax)
+let price = taxCalculator(originalPrice, tax)
 document.getElementById('content').innerHTML = price + '円'
